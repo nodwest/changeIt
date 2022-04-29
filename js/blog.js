ade1fd2bd09f9b724c034f09a08cd40059e18659
nodwest <38777395+nodwest@users.noreply.github.com>
2022-04-29 20:14:19 +0300

    add SORT ABC

--- js/blog.js
+++ js/blog.js
@@ -4,44 +4,140 @@ import { ValidationName, errosMessage } from './form.js'
 
 const formBlog = document.getElementById('blog')
 const formBlogInputs = formBlog.getElementsByTagName('input')
-
 const regTitle = /[a-zA-Zа-яА-Я]{3,25}/
 const regText = /[a-zA-Zа-яА-Я]{3,100}/
 formBlogInputs[0].addEventListener('change', ValidationName(errosMessage[4], regTitle))
 formBlogInputs[1].addEventListener('change', ValidationName(errosMessage[5], regText))
 
-const blogTitle = document.querySelector('.blog__title')
+const createNewPost = (post) => {
+    const newPosts = document.createElement('div')
+    newPosts.classList.add('asnwer')
 
-const createNewPost = () => {
-    const newPost = document.createElement('div')
-    newPost.classList.add('asnwer__item')
+    post.forEach(item => {
 
-    newPost.innerHTML = `
-    <div class="asnwer__item__title">
-         ${formBlogInputs[0].value}  
-    </div>
+        newPosts.innerHTML += `
+        <div class="asnwer__item">
+            <div class="asnwer__item__title">
+                ${item.title}  
+            </div>
 
-    <div class="asnwer__item__text">
-        ${formBlogInputs[1].value}  
-    </div>
-    `
-    document.querySelector('.blog-answer-field').append(newPost)
+            <div class="asnwer__item__text">
+                Decription : ${item.text}  
+            </div>
+
+            <div class="asnwer__item__text">
+                Date : ${item.date}  
+            </div>
+
+        </div>
+        `
+    });
+
+    document.querySelector('.blog-answer-field').append(newPosts)
 }
 
 const posts = [
     {
-        title : 'any title',
-        text : 'any text'
+        title: 'New Live',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores repellat facere ut. Et, voluptatum nobis?',
+        date: '09/10/2018'
+    },
+    {
+        title: 'A My Live',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores repellat facere ut. Et, voluptatum nobis?',
+        date: '11/10/2018'
     }
-] 
+]
+
+createNewPost(posts)
 
 const createNewPostObj = () => {
+    const date = new Date
     const post = {
-        title : formBlog[0].value,
-        text : formBlog[1].value
+        title: formBlog[0].value,
+        text: formBlog[1].value,
+        date: date
     }
     posts.push(post)
 }
+
+// Remove
+const removeAnswers = () => {
+    const answers = document.getElementsByClassName('asnwer')
+    for (let answer of answers) {
+        answer.remove()
+    }
+}
+// Search Post
+const inputSeach = document.querySelector('.blog__search')
+inputSeach.addEventListener('input', (e) => {
+    if (e.target.value == '') {
+        removeAnswers()
+        createNewPost(posts)
+    }
+
+    else {
+        const filterPost = posts.filter(item => {
+            if (item.title.toLowerCase().indexOf(e.target.value) != -1) {
+                return item
+            }
+        })
+        removeAnswers()
+        createNewPost(filterPost)
+    }
+})
+
+// Sort ABC 
+let flag = true
+const btnSortAbc = document.querySelector('.sort-abc')
+btnSortAbc.addEventListener('click', () => {
+
+    const filterABC = posts.sort((a, b) => {
+
+        if (a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase()) {
+            return -1
+        }
+        if (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase()) {
+            return 1
+        }
+        return 0
+    })
+
+    // removeAnswers()
+    // createNewPost(filterABC)
+    if (flag) {
+        const filterABC = posts.sort((a, b) => {
+
+            if (a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase()) {
+                return -1
+            }
+            if (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase()) {
+                return 1
+            }
+            return 0
+        })
+
+        removeAnswers()
+        createNewPost(filterABC)
+    }
+
+    else {
+        const filterCBA = posts.sort((a, b) => {
+
+            if (a.title.toLocaleLowerCase() > b.title.toLocaleLowerCase()) {
+                return -1
+            }
+            if (a.title.toLocaleLowerCase() < b.title.toLocaleLowerCase()) {
+                return 1
+            }
+            return 0
+        })
+        removeAnswers()
+        createNewPost(filterCBA)
+    }
+
+    flag = !flag
+})
 
 // ADD NEW POST
 formBlogInputs[2].addEventListener('click', (e) => {
@@ -58,9 +154,9 @@ formBlogInputs[2].addEventListener('click', (e) => {
                 ++count
             }
             if (count == formBlog.length - 1) {
-                createNewPost() 
                 createNewPostObj()
-                console.log(posts)
+                removeAnswers()
+                createNewPost(posts)
                 break
             }
         }
